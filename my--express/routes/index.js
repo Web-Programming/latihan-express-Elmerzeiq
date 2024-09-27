8cc3582c7e7f2ba0bd6124c0842bb7bb123c2c8c
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-09-27 18:26:10 +0700

    comit

--- my--express/routes/index.js
+++ my--express/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Expressi' });
 });
 
+router.get("/about", function(req, res, next){
+  res.render('about',{title: "About Us"});
+});
 module.exports = router;
