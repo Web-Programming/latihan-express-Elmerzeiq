b701e6efac4eb794f93f335140f920e9888828b8
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-10-09 17:53:29 +0700

    comit

--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -6,9 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/paketwisata', function(req, res, next) {
-  res.render('paketwisata', { title: 'Paket Wisata' });
+router.get('/paketwisata', (req, res) => {
+  const paketTidung = [
+      { peserta: "2", hargaBoatFery2H1M: "Rp 965.000", hargaBoatFery3H2M: "Rp 1.125.000", hargaSpeedboat2H1M: "Rp 1.055.000", hargaSpeedboat3H2M: "Rp 1.205.000", hargaBahariExpress2H1M: "Rp 865.000", hargaBahariExpress3H2M: "Rp 1.215.000" },
+      { peserta: "3", hargaBoatFery2H1M: "Rp 915.000", hargaBoatFery3H2M: "Rp 1.075.000", hargaSpeedboat2H1M: "Rp 1.005.000", hargaSpeedboat3H2M: "Rp 1.155.000", hargaBahariExpress2H1M: "Rp 815.000", hargaBahariExpress3H2M: "Rp 1.165.000" },
+      // Tambahkan data lainnya
+  ];
+
+  const fasilitas = [
+      "Tiket Boat Pulang & Pergi",
+      "Penginapan (AC, TV, Dispenser, View Pantai)",
+      "Makan: 3x (2D 1N via Boat Fery), 4x (2D 1N via Speedboat), 6x (3D 2N)",
+      "Barbeque 1x",
+      "Sepeda",
+      "Banana Boat / Sofa Boat 1x",
+      "Boat Snorkeling Privat",
+      "Dokumentasi Underwater",
+      "Service Guide"
+  ];
+  res.render('paketwisata', { paketTidung, fasilitas });
 });
+
 
 router.get('/orderpaket', function(req, res, next) {
   res.render('orderpaket', { title: 'Form pemesanan paket' });
