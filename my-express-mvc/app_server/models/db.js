2f843bd0285298d1fa16c3cbc5262ba1cdccab93
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-10-17 10:10:19 +0700

    comit

---
+++ my-express-mvc/app_server/models/db.js
@@ -0,0 +1,16 @@
+let mongoose = require("mongosee");
+let dbURL = "mongodb://localhost:27017"
+mongoose.connect(dbURL,{
+useNewURLParser : true
+});
+
+mongoose.connection.on("connected",() => {
+console.log("Connected To MongoDB");
+});
+
+mongoose.connection.on("error",(error);
+});
+
+mongoose.connection.on("disconnected"),() => {
+console.log("Disconnected From MongoDB");
+});
