bf44d8bad856bf236446fe92530bf6a82e34aa99
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-10-23 22:39:24 +0700

    insert

--- my-express-mvc/app.js
+++ my-express-mvc/app.js
@@ -4,13 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-
-
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var mahasiswasRouter = require('./app_server/routes/mahasiswas');
 require('./app_server/models/db');
+var mahasiswasRouter = require ('./app_server/routes/mahasiswas');
+
 var app = express();
 
 // view engine setup
@@ -26,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', mahasiswasRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
