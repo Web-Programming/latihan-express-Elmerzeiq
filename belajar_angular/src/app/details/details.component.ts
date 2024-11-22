35ef1c07d6c08f1dd4be8e3a4dedbcc1e0532515
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-11-22 13:32:18 +0700

    penambahan housing

--- belajar_angular/src/app/details/details.component.ts
+++ belajar_angular/src/app/details/details.component.ts
@@ -1,68 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <p>
-      details works for housingLocationId: {{ housingLocationId }}
-    </p>
-
-    <section>
-      <h1 class="section-heading">Apply to Live Here</h1>
-      <form [formGroup]="applyForm" (ngSubmit)="submitApplyForm()" novalidate>
-        <div>
+     <article >
+      <img class="listing-photo" [src]="housingLocation?.photo"
+        alt="Exterior photo of {{housingLocation?.name}}"/>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h1 class="section-heading">Apply to live here</h1>
+        <form [formGroup]="applyForm" (submit)="submitApplyForm()">
           <label for="first-name">First Name</label>
-          <input type="text" id="first-name" formControlName="firstName" placeholder="Input First Name">
-          <div *ngIf="applyForm.get('firstName')?.invalid && applyForm.get('firstName')?.touched" class="error">
-            First Name is required.
-          </div>
-        </div>
-
-        <div>
+          <input type="text" id="first-name" formControlName="firstName" placeholder="Input first name">
           <label for="last-name">Last Name</label>
-          <input type="text" id="last-name" formControlName="lastName" placeholder="Input Last Name">
-          <div *ngIf="applyForm.get('lastName')?.invalid && applyForm.get('lastName')?.touched" class="error">
-            Last Name is required.
-          </div>
-        </div>
-
-        <button type="submit" [disabled]="applyForm.invalid">Apply</button>
-      </form>
-    </section>
+          <input type="text" id="last-name" formControlName="lastName" placeholder="Input last name">
+          
+          <label for="email">Email</label>
+          <input type="email" id="email" formControlName="email" placeholder="Input email">
+          <button type="submit" class="primary">Apply</button>
+        </form>
+      </section>
+    </article>
   `,
-  styleUrls: ['./details.component.css']
+  styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-  housingLocationId: number = 0;
-  housingLocation: HousingLocation | undefined;
-
-  // Form with basic validation (required fields)
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined
   applyForm: FormGroup = new FormGroup({
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required])
-  });
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
 
-  constructor(
-    private route: ActivatedRoute,
-    private housingService: HousingService
-  ) {
+  constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
-    console.table(this.housingLocation);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
+    console.table(this.housingLocation)
   }
 
   submitApplyForm() {
-    if (this.applyForm.valid) {
-      alert('First Name: ' + this.applyForm.value.firstName + ', Last Name: ' + this.applyForm.value.lastName);
-    } else {
-      alert('Please fill out the form correctly');
-    }
+    //panggil API simpan data registarsi via service
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
   }
 }
--- my-express-mvc/app_server/models/db.js
+++ my-express-mvc/app_server/models/db.js
@@ -1,14 +1,14 @@
-let mongoose=require("mongoose")
-//let dbURI ='mongodb://localhost:27017/pawII-si52';
-let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
-mongoose.connect(dbURI,{
-   // useNewURLParser: true
+let mongoose = require("mongoose");
+let dbURL = "mongodb://localhost:27017/pawSI52";
+
+mongoose.connect(dbURL, {
+   //useNewURLParser: true
 });
 
-mongoose.connection.on("connected",() => {
- console.log("connected to MongoDB");
+mongoose.connection.on("connected", () => {
+   console.log("connected to MongoDB");
 });
 
 mongoose.connection.on("error", (error) => {
-console.log("disconnected from MongoDB");
+   console.log("disconnected from MongoDB");
 });
