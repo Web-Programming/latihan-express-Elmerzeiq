08d5588c7df02f64083efd21ec827e1c809c5c6c
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-11-29 18:36:21 +0700

    UPDATE : TUGAS PAW

--- my-express-mvc/app_server/routes/register.js
+++ my-express-mvc/app_server/routes/register.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const registerController = require("../controllers/controllerRegister");
+const registerController = require("../controllers/controllerRegister.js");
 
 // Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data dalam Database MongoDB
-// router.get("/", registerController.Index);
+//router.get("/", registerController.Index);
 router.post("/register", registerController.Insert);
 module.exports = router;
