d20438b3c1292dc2f957b4e7a247e3e5587920b3
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-12-04 20:28:14 +0700

    register

---
+++ belajar_angular/src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  formError: String = "";
+  loginForm: FormGroup;
+
+  //Inject class Router dan service authentication  
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  public onLoginSubmit(): void {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      console.log('Form submitted', formData);
+      //Panggil method loginAuth()
+
+    } else {
+      this.formError = 'All fields are required, please try again';
+    }
+  }
+}
